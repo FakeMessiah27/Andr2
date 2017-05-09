a3ba0277199db38a30894c7e2a4f5d40e4d000c4
Gheorghe Savin,Cezar C <c.gheorghesavin@student.fontys.nl>
2017-05-09 14:13:48 +0200

    Implementation
    Added the best method ever. Dank code moite.

--- Android2Application/app/src/main/java/com/teamawesome/android2application/MainActivity.java
+++ Android2Application/app/src/main/java/com/teamawesome/android2application/MainActivity.java
@@ -10,4 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void bestMethodEver()
+    {
+        System.out.print("best. method. ever.");
+    }
 }
