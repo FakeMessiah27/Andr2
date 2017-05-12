7db50bb1bfc12859188797f7e8865518f939eb6b
Gheorghe Savin,Cezar C <c.gheorghesavin@student.fontys.nl>
2017-05-12 15:43:27 +0200

    Initial Development
    Created the main activities:
    -LogInActivity
    - textboxes are for presentation purposes
    - button opens the main activity
    -MainActivity
    -profile pic image (with onclicklistener)
    -random map image for presentation purposes
    -ProfileActivity
    - default android icon
    - not a priority yet

--- Android2Application/app/src/main/java/com/teamawesome/android2application/MainActivity.java
+++ Android2Application/app/src/main/java/com/teamawesome/android2application/MainActivity.java
@@ -1,7 +1,11 @@
 package com.teamawesome.android2application;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageView profilePic = (ImageView)findViewById(R.id.profileView);
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 
     public void worstMethodEver()
