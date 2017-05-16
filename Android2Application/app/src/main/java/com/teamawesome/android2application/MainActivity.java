c19fc8f14931b5a9a22236b555943ce8d8038d4f
Gheorghe Savin,Cezar C <c.gheorghesavin@student.fontys.nl>
2017-05-16 13:25:15 +0200

    Revert "Revert "Menu Item""
    This reverts commit 14da4aba44b296cc07ee98f5708c5f857088864d.

--- Android2Application/app/src/main/java/com/teamawesome/android2application/MainActivity.java
+++ Android2Application/app/src/main/java/com/teamawesome/android2application/MainActivity.java
@@ -2,8 +2,12 @@ package com.teamawesome.android2application;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -13,24 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu); //your file name
+        return super.onCreateOptionsMenu(menu);
+    }
 
-        ImageView profilePic = (ImageView)findViewById(R.id.profileView);
-        profilePic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.MenuProfile:
+                intent = new Intent(this, ProfileActivity.class);
                 startActivity(intent);
-            }
-        });
-
-
-
-
+                return true;
+            case R.id.MenuLogOut:
+                intent = new Intent(this, LogInActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
-    public void worstMethodEver()
-    {
-        System.out.print("worst. method. ever.");
-    }
 }
